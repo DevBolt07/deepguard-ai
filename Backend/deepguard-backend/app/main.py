4f4c58d0780d950a9930bf943e3aa2cdfdd8eb8a
Viraj-1124 <rushikeshs200624@gmail.com>
2025-12-12 21:01:24 +0530

    Connected backend and frontend

--- Backend/deepguard-backend/app/main.py
+++ Backend/deepguard-backend/app/main.py
@@ -3,9 +3,17 @@ from app.api.v1.video import router as video_router
 from app.api.v1.audio import router as audio_router
 from app.api.v1.link import router as link_router
 from app.api.v1.image import router as image_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="DeepGuard AI Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # you can restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(video_router)
 app.include_router(audio_router)
